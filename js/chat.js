8ea3ebe4427fb3be7b27c62b1d84ac4ee3d593bf
Darryl Forbes <darrylforbes2022@u.northwestern.edu>
2020-02-26 01:59:37 -0600

    Made chat look nicer

--- js/chat.js
+++ js/chat.js
@@ -1,4 +1,19 @@
 const postMessage = () => {
+  let message = document.querySelector("#input").value;
+  document.querySelector("#input").value = "";
+  let div_elem = document.createElement("div");
+  div_elem.className = "message user";
+  let p_elem = document.createElement("p");
+  div_elem.appendChild(p_elem);
+
+  document.querySelector("#messages").appendChild(div_elem);
+  p_elem.innerText = message;
+
+  scrollDown();
+  window.setTimeout(postResponse, 1000);
+};
+
+const postResponse = () => {
   let message_arr = [
     "Hi!",
     "I hope you are doing great today!",
@@ -6,24 +21,21 @@ const postMessage = () => {
     "Everything's going to be okay!"
   ];
 
-  let message = document.querySelector("#input").value;
-  document.querySelector("#input").value = "";
-  let div_elem = document.createElement("div");
-  div_elem.className = "user";
-  let p_elem = document.createElement("p");
-  div_elem.appendChild(p_elem);
-
-  document.querySelector("#messages").appendChild(div_elem);
-  p_elem.innerText = message;
-
   let response_div = document.createElement("div");
   let response_p = document.createElement("p");
-  response_div.className = "response";
+  response_div.className = "message response";
   response_div.appendChild(response_p);
 
   let rand_c = Math.floor(Math.random() * message_arr.length);
   document.querySelector("#messages").appendChild(response_div);
   response_p.innerText = message_arr[rand_c];
+
+  scrollDown();
+};
+
+const scrollDown = () => {
+  const messages = document.querySelector("#messages");
+  messages.scrollTop = messages.scrollHeight - messages.clientHeight;
 };
 
 document.addEventListener("DOMContentLoaded", function() {
